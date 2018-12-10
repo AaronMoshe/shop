ace5d43927f4a9da86aded399170234176a5d385
Sadoun Aaron <aarons@MacBook-Pro-de-Sadoun.local>
2018-12-10 17:34:08 +0200

    first commit

---
+++ shop_project/shop_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Product(models.Model):
+	name = models.CharField(max_length=264)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	description = models.TextField()
+	image = models.ImageField(default='static/images/nike_air.jpeg', upload_to='static/images/')
+
+	def __str__(self):
+		return self.name
+
+	def __repr__(self):
+		return "<Product {}>".format(self.name)
+
+class Client(models.Model):
+	first_name = models.CharField(max_length=264)
+	last_name = models.CharField(max_length=264)
+	email = models.EmailField(max_length=264, unique=True)
+	password = models.CharField(max_length=264)
+	profile_picture = models.ImageField(default='static/images/nike_air.jpeg', upload_to='static/images/profile_pictures')
+
+	def __str__(self):
+		return self.email
+
+	def __repr__():
+		return "<Client {}".format(self.email)
