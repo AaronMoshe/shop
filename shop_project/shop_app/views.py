ce587c9f6e81cd724b1064547261f2e45dde9176
Sadoun Aaron <aarons@MacBook-Pro-de-Sadoun.local>
2018-12-12 17:31:49 +0200

    creations de la fonctionnalite commentaire

--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
-from shop_app.models import Product, Client
+from shop_app.models import Product, Client, Comment
+from shop_app.forms import CommentForm
+import datetime
 
 
 def index(request):
-	products = Product.objects.all()[20]
+	products = Product.objects.all()
 	return render(request, 'index.html', context={'products': products})
 
 def product(request, product_id):
 	product = Product.objects.get(id=product_id)
-	return render(request, 'product.html', context={'product':product})
+	comments = Comment.objects.filter(product_id=product.id)
+	return render(request, 'product.html', context={'product':product, 'comments': comments})
 
 def clients(request):
-	clients = Client.objects.all()[20]
+	clients = Client.objects.all()
 	return render(request, 'clients.html', context={'clients': clients})
 
 def client(request, client_id):
 	client = Client.objects.get(id=client_id)
 	return render(request, 'client.html', context={'client': client})
+
+def comment_form(request, product_id):
+	if request.method == 'POST':
+		comment = Comment.objects.get_or_create(username=request.POST.get('username'), text=request.POST.get('text'), date=datetime.datetime.now(), product=Product.objects.get(id=product_id))
+
+	comment_form = CommentForm()
+	return render(request, 'comment_form.html', context={'comment_form': comment_form})
